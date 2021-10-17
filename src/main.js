d0baf49db3ca367fe95b52a2091896cd3ec01d11
acl.gabriel <acl.gabriel@gmail.com>
2021-10-17 18:15:31 +0200

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import axios from 'axios';
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import VueLazyload from 'vue-lazyload'
 
+
+Vue.use(VueLazyload, {
+  preLoad: 1.3,
+  error: 'not-available.jpg',
+  loading: 'loading-spin.svg',
+  attempt: 1,
+  lazyComponent : true,
+  listenEvents: [ 'scroll' ]
+});
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 Vue.config.productionTip = false;
+Vue.prototype.$axios = axios
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,91 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container-sm">
+    <b-overlay :show="loading" no-wrap>    </b-overlay>
+
+      <div class="row mt-4" v-lazy-container="{ selector: 'img' }">
+        <!-- border -->
+        <div class=" col-md-2 col-sm-3 col-6 mb-2 px-1 bg-ligh item-content " v-for="(img,index) in images" v-bind:key="index"  @click="deleteItem(index)">
+              <div class=" d-flex justify-content-center align-items-center box" >
+                <img :data-src="img.url"  class="lazy-img">
+                  <span class="position-absolute d-none delete-item">
+               <div >
+                  <div class="d-flex justify-content-center align-items-center">
+                    <b-icon icon="trash" variant="dark" font-scale="2"></b-icon> 
+                  </div>
+                  <div class="font16 text-dark fw-bold">Eliminar</div>
+               </div>
+              </span>
+              </div>
+            
+        </div>
+      </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "Home",
   components: {
-    HelloWorld,
   },
+  data(){
+    return {
+      images : [],
+      loading : false
+    }
+  },
+  created(){
+    this.onLoadImages();
+  },
+  methods : {
+     onLoadImages(){
+      this.loading = true;
+      this.$axios("http://jsonplaceholder.typicode.com/photos")
+        .then((response) => {
+            this.images = response.data;
+            this.loading = false;
+        })
+        .catch(()=>{
+            this.loading = false;
+
+        })
+    },
+    deleteItem(index){
+      this.loading = true;
+      this.images.splice(index,1);
+      this.loading = false;
+    }
+  }
 };
 </script>
+<style>
+/* .box{
+  min-height:250px;
+} */
+.item-content{
+  cursor: pointer;
+
+}
+.item-content:hover{
+  opacity: 0.8;
+}
+.item-content:hover > .box .delete-item{
+  display: block !important;
+}
+.font16{
+  font-size: 16px;
+}
+.lazy-img{
+  max-width: 100%;
+}
+.lazy-img[lazy=loading],.lazy-img[lazy=error] {
+    margin: 100px 0;
+    width: 50px !important;
+}
+.lazy-img[lazy=loaded] {
+    -webkit-animation-duration: 1s;
+    animation-duration: 1s;
+    -webkit-animation-fill-mode: both;
+    animation-fill-mode: both;
+    -webkit-animation-name: fadeIn;
+    animation-name: fadeIn;
+}
+</style>
